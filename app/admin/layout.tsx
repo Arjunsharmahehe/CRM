294f634996ffb4f1e173384d16d9447e7b992092
Arjun Sharma <whocaresaboutarjun@gmail.com>
2025-12-07 15:44:48 +0530

    forgot to press ctrl+s

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import SidebarNav from "@/app/admin/SidebarNav";
+import SidebarNav from "@/components/SidebarNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
